369118645731002711bb4826ce9b1fbaee2c8356
mansourbash <mansour@zenhr.com>
2023-08-28 05:54:17 +0300

    change configration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains:['lh3.googleusercontent.com','firebasestorage.googleapis.com']
+    domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com']
   },
-}
 
-module.exports = nextConfig
+  output: {
+    // Set your target directory for the exported files
+    // For example, you can use 'out' or any other directory name
+    directory: 'out',
+  },
+};
+
+module.exports = nextConfig;
